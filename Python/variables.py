9fe39f221369b4a0d1ba55d7a65a5c235bea257e
Nitish Tiwari <nitishtiwari.edu@gmail.com>
2026-05-17 22:53:15 +0545

    Variables and datatypes in python

---
+++ Python/variables.py
@@ -0,0 +1,20 @@
+user_name = "John"
+user_age = 26
+user_course = "Python"
+is_enrolled = True
+
+# How they will be displayed
+
+print(user_name)
+print(user_age)
+print(user_course)
+print(is_enrolled)
+
+# What their types are
+
+print(type(user_name))
+print(type(user_age))
+print(type(user_course))
+print(type(is_enrolled))
+
+# The type command displays the datatype of the variable
