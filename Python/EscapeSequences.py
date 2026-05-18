18d86df1276555bfb79a016ffa5af1904def7fd9
Nitish Tiwari <nitishtiwari.edu@gmail.com>
2026-05-18 23:45:09 +0545

    Escape Sequences in python

---
+++ Python/EscapeSequences.py
@@ -0,0 +1,11 @@
+# This is a comment 
+# \\" This escape sequence is used to add a double quote
+# \\' This escape sequence is used to add a single quote
+# \\ This escape sequence is used to add a backslash
+# \\n This escape sequence is used to add a new line to the characters afterwards
+
+course = "Python \"Programming"
+print(course)
+
+test = "Python \n Programming"
+print(test)
