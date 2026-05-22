979f7121583962ba9a94735ffe4b5642dc6597da
Nitish Tiwari <nitishtiwari.edu@gmail.com>
2026-05-22 23:04:01 +0545

    Arithmetic Operations in Python

---
+++ Python/numbers.py
@@ -0,0 +1,13 @@
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3) #For dividing and the result will be in Float
+print(10 // 3) #For dividing and the result will be in Integer
+print(10 % 3)
+
+x = 10
+print(x)
+x = 10+3
+print(x)
+x += 3
+print(x)
