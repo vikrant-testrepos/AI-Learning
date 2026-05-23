15304e814f9ba735e2083158f37710d77f94fbed
Nitish Tiwari <nitishtiwari.edu@gmail.com>
2026-05-23 23:35:14 +0545

    Conditional statements

---
+++ Python/conditional_statements.py
@@ -0,0 +1,9 @@
+age = 12
+if age > 18:
+    print("You are free to watch any movie")
+    print("You can have drink too")
+elif age >= 16:
+    print("You must watch under 16 categories")
+else:
+    print("You are a kid just go to your school and study")
+print("Done")
