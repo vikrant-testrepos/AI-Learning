19d73c8f7a6ec5c8f86b09e895977e7fd6fd01b0
Nitish Tiwari <nitishtiwari.edu@gmail.com>
2026-05-24 14:57:06 +0545

    For loop

---
+++ Python/for_loop.py
@@ -0,0 +1,8 @@
+for number in range(3):
+    print("Attempt", number + 1, (number + 1) * ".") 
+
+for number in range(1,4):
+    print("Attmpt", number, number * ".")
+
+for number in range(1,10,2):
+    print("Attempt", number, number * ".")
