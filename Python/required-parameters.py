eacb7382fdadfdda8e2b5d50f82ae12e3497c1b0
Nitish Tiwari <nitishtiwari.edu@gmail.com>
2026-06-04 22:57:25 +0545

    Default parameters

---
+++ Python/required-parameters.py
@@ -0,0 +1,5 @@
+def increment(number, by=1): #If we don't pass the second value then the default value will be used and the default parameter should be defined after the required parameters.
+    return number + by
+
+
+print(increment(2))
